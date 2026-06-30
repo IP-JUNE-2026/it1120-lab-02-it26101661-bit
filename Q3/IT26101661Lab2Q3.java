fa6a3ba57273a4433885026b5ed2cb08d46b6672
it26101661-bit <it26101661@my.sliit.lk>
2026-07-01 01:08:26 +0530

    Add IT26101661Lab2Q3.java to Q3

---
+++ Q3/IT26101661Lab2Q3.java
@@ -0,0 +1,13 @@
+public class IT26101661Lab2Q3 {
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+        double hypotenuse;
+
+        // Calculate the hypotenuse
+        hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Length of the hypotenuse: " + hypotenuse);
+    }
+}
