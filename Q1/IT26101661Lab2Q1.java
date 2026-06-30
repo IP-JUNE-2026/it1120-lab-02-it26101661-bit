b52651333fd4da4b44ff4a46e8db36d2b6cffbe6
it26101661-bit <it26101661@my.sliit.lk>
2026-07-01 01:07:01 +0530

    Add IT26101661Lab2Q1.java to Q1

---
+++ Q1/IT26101661Lab2Q1.java
@@ -0,0 +1,14 @@
+public class IT26101661Lab2Q1 {
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+        double length, width;
+
+        // Calculate the length and width
+        length = (perimeter * 2) / 7;
+        width = (3.0 / 4.0) * length;
+
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
